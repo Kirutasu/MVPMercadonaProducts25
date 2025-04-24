dbe4496db47e2a0f123b28e0205de3ccaeeda9ab
Kirutasu <125732333+Kirutasu@users.noreply.github.com>
2025-04-24 17:35:32 +0200

    Proyecto iniciado con Spring y dependencias: Web, JPA, H2 Database, Security

---
+++ src/test/java/com/mercadonaProductos/productosAPI/ProductosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadonaProductos.productosAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
